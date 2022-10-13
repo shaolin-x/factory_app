92650fa0569248d9214216b052a3fa9d7e5d8fb2
shaolin-x <xshaolin123@gmail.com>
2022-10-13 01:40:33 -0400

    finished

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
+function App() {
+  const [userData, setUserData] = useState([])
+  const getUserData = ()=>{
+    axios.get('https://cs519app.azurewebsites.net/api/showDB?')
+    .then((res)=>{
+      setUserData(res.data)
+    })
+  }
+
+  useEffect(()=>{
+    getUserData();
+  },[])
+
+  
+
+  return (
+    <div className="App">
+      <table>
+          <tr>
+            <th>Product Name</th>
+            <th>Product ID</th>
+            <th>Quantity Received</th>
+            <th>Quantity Shipped</th>
+            <th>Quantity Returned</th>
+            <th>Quantity on Hand</th>
+          </tr>
+      {userData && userData.map((data)=>(
+        
+          <tr>
+            <td>{data.ProductName}</td>
+            <td>{data.ProductID}</td>
+            <td>{data.numReceived}</td>
+            <td>{data.numShipped}</td>
+            <td>{data.numReturen}</td>
+            <td>{data.numStorage}</td>
+          </tr>
+       
+      ))}
+      </table>
+
+    </div>
+  );
+}
+
+export default App;
